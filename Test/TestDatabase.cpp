655ae59686520ee79813ffa7879c2351944fdb39
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2015-01-14 01:42:12 -0500

    add special test case for RDatabase
    Change-Id: I3cf85ebd285b8ee73458bbcaefcd5bb74621f4ed

--- Test/TestDatabase.cpp
+++ Test/TestDatabase.cpp
@@ -18,6 +18,7 @@ bool TestRDatabase()
 	shared_ptr<RObject> person2 = make_shared<RObject>(_T("Sofya"), _T(""));
 	person2->SetAttribute(_T("Age"), _T("30"));
 	person2->SetAttribute(_T("Height"), _T("5'4\""));
+	person2->SetAttribute(_T("SpecialTest"), _T("and='&'; greater_than='>'; less_than='<'; space:' '"));
 	db1.Set(person1);
 	db1.Set(person2);
 	assert( db1.Save(_T("Test\\TestRDatabase1.txt")) );	// note: directory Test must be existed
@@ -27,5 +28,7 @@ bool TestRDatabase()
 	assert(db2->Get(_T("Ray")) != person1);
 	assert(*db2->Get(_T("Ray")) == *person1);
 	assert(*db2->Get(_T("Sofya")) == *person2);
+	assert((*db2->Get(_T("Ray")))[_T("Contact")][_T("cell")].GetValue() == _T("123-456-7890"));
+	assert(db2->Get(_T("Sofya"))->GetAttribute(_T("SpecialTest"))->GetValue() == _T("and='&'; greater_than='>'; less_than='<'; space:' '"));
 	return true;
 }
